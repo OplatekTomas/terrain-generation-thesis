f52b42ad35c6f1c3c19c15b505339800edc23b9d
Tomáš Oplatek <tomas.oplatek@gmail.com>
2021-10-03 21:22:13 +0200

    Incredibly bad triangle rendering + neat shader including

--- src/gui/opengl/Renderer.cpp
+++ src/gui/opengl/Renderer.cpp
@@ -4,6 +4,70 @@
 
 #include "Renderer.h"
 
+#include <iostream>
+#include <QOpenGLContext>
+#include "shaders/Shaders.h"
+
 Renderer::Renderer(QWidget* parent) : QOpenGLWidget(parent) {
 
+}
+
+
+void Renderer::tempSetup(){ //Testing setup
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f,  0.5f, 0.0f
+    };
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &VertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &FragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+void Renderer::initializeGL() {
+    std::cout << "Init" << std::endl;
+    initializeOpenGLFunctions();
+    glClearColor(1.0,1.0,1.0,1.0);
+
+    tempSetup();
+
+    QOpenGLWidget::initializeGL();
+}
+
+void Renderer::paintGL() {
+    glClearColor(0.0,0.0,0.0,0.0);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    std::cout << "Paint" << std::endl;
+    QOpenGLWidget::paintGL();
+}
+
+void Renderer::resizeGL(int w, int h) {
+    std::cout << "Resize" << std::endl;
+    QOpenGLWidget::resizeGL(w, h);
 }
--- src/gui/opengl/Renderer.h
+++ src/gui/opengl/Renderer.h
@@ -6,17 +6,26 @@
 #define VUT_BP_RENDERER_H
 #include <QOpenGLWidget>
 #include <MainWindow.h>
+#include <QOpenGLFunctions>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Renderer; }
 QT_END_NAMESPACE
 
 
-class Renderer : public QOpenGLWidget  {
+class Renderer : public QOpenGLWidget, protected QOpenGLFunctions  {
 Q_OBJECT
 
 public:
     Renderer(QWidget *parent = nullptr);
+private:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int w, int h) override;
+    QOpenGLFunctions* ctx;
+
+    void tempSetup();
 };
 
 
